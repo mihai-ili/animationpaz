88a4c32cc81c8eb7c4703cdc794cd3ef60d5d3b1
mihai <mikhail_ilichev24@mail.ru>
2024-12-19 10:59:49 +0300

    анимации

---
+++ app/src/main/java/com/example/animation/MainActivity3.java
@@ -0,0 +1,42 @@
+package com.example.animation;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity3 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main3);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+
+    public void klick3(View view) {
+        ImageView image = findViewById(R.id.imageView3);
+        Animation animation = AnimationUtils.loadAnimation(this, R.anim.translate);
+        image.startAnimation(animation);
+
+    }
+
+    public void anim4(View view) {
+        Intent intent = new Intent(MainActivity3.this, MainActivity4.class);
+        startActivity(intent);
+        finish();
+    }
+}
